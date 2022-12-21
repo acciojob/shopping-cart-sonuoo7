2daec10071ca21f11cf440903bf185ea58125314
Sonu Pant007 <60938647+sonuoo7@users.noreply.github.com>
2022-12-21 11:09:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your code here
+let priceArray = [];
+let acc = 0;
+
+function addData() {
+    const price = document.getElementById('item-price-input').value;
+    console.log(price);
+
+    const productName = document.getElementById('item-name-input').value;
+    console.log(productName);
+    
+    priceArray.push(Number(price));
+    let total = priceArray.reduce(add, acc);
+
+    let priceChange = document.getElementById('price-change');
+    priceChange.innerHTML = total;
+
+    let tableRef = document.getElementById('add-rows');
+    let newRow = tableRef.insertRow(-1);
+    let newCellOne = newRow.insertCell(0);
+    let newCellTwo = newRow.insertCell(1);
+
+    let productNameText = document.createTextNode(productName);
+    let productPriceText = document.createTextNode(price);
+
+    newCellOne.appendChild(productNameText);
+    newCellTwo.appendChild(productPriceText);
+
+}
+
+function add(acc, a) {
+    return acc+a;
+}//your code here
